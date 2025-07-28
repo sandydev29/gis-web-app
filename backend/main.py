311b7601d6270413f80a0f547f2237561ff7f410
sandydev.29 <sandeshdevaramane@gmail.com>
2025-07-28 20:34:40 +0530

    Updated at 28

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,15 @@ def get_names():
     conn.close()
     return names
 
+@app.get("/get-all")
+def get_names1():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT name FROM points ORDER BY name;")
+    names = [row[0] for row in cur.fetchall()]
+    conn.close()
+    return names
+
 @app.get("/get-bng")
 def get_names():
     conn = get_connection()
@@ -72,6 +81,31 @@ def get_points(name: str):
             FROM points p
             JOIN bengaluru poly ON poly.subroad = %s
             WHERE ST_Within(p.geom, poly.geom);
+        """, (name,))
+        result = cur.fetchone()[0]
+        print("Result:", result)  # Debug print
+
+        if result is None:
+            return {"error": f"No data found for district: {name}"}
+        return result
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        conn.close()
+
+@app.get("/get-searchpoints")
+def get_points(name: str):
+    conn = get_connection()
+    cur = conn.cursor()
+    print(f"Fetching points for district: {name}")  # Debug print
+    try:
+        cur.execute("""
+            SELECT jsonb_build_object(
+                'type', 'FeatureCollection',
+                'features', jsonb_agg(ST_AsGeoJSON(p.*)::jsonb)
+            )
+            FROM points p
+            WHERE name = %s;
         """, (name,))
         result = cur.fetchone()[0]
         print("Result:", result)  # Debug print
